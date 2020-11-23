f096d64aae0805cb54a32ae941a0a1c8cd5ed142
tschaffter <thomas.schaffter@gmail.com>
2020-11-22 16:45:30 -0800

    Fix so that the app can start with docker

--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -8,5 +8,4 @@ from openapi_server.models.note import Note
 from openapi_server.models.service import Service
 from openapi_server.models.text_annotation import TextAnnotation
 from openapi_server.models.text_physical_address_annotation import TextPhysicalAddressAnnotation
-from openapi_server.models.text_physical_address_annotation_all_of import TextPhysicalAddressAnnotationAllOf
 from openapi_server.models.text_physical_address_annotations import TextPhysicalAddressAnnotations
